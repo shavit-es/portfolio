c45b4041adff06898b9d2ed384743a486144b1e8
shavit-es <shavit0423@naver.com>
2021-10-21 10:58:11 +0000

    10.21 start

---
+++ commonpart.php
@@ -0,0 +1,47 @@
+<?php
+$navbar='<p class="text-center m-3">
+			<a class="toptitle" href="index.php">이현희 포트폴리오 사이트</a>
+		</p>
+		<ul class="nav justify-content-center">
+		<li class="nav-item">
+			<a class="nav-link active" aria-current="page" href="announcement.php">공지사항</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link actice" href="menu.php" tabindex="-1" aria-disabled="true">메뉴</a>
+		</li>
+		<li class="nav-item">
+			<a class="nav-link" href="inquiry.php">식수조사</a>
+		</li>
+		</ul>';
+$headpart='
+	<link
+		href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+		rel="stylesheet"
+		integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We"
+		crossorigin="anonymous"
+	/>
+	<link rel="stylesheet" type="text/css" href="/css/style.css" />
+	<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta charset="utf-8" />
+		<meta name="robots" content="index,follow" />
+		<title>이현희 포트폴리오 사이트</title>
+		<meta name="description" content="이현희 포트폴리오 사이트" />
+		<meta property="og:title" content="이현희 포트폴리오" />
+		<meta property="og:description" content="이현희 포트폴리오" />
+		<meta property="og:image" content="images/android-chrome-512x512.png" />
+		<!-- <meta property="og:url" content="https://brandtest.site/" /> -->
+		<link rel="apple-touch-icon" sizes="180x180" href="images/apple-touch-icon.png" />
+		<link rel="icon" type="image/png" sizes="32x32" href="images/favicon-32x32.png" />
+		<meta name="NaverBot" content="All" />
+		<meta name="NaverBot" content="index,follow" />
+		<meta name="Yeti" content="All" />
+		<meta name="Yeti" content="index,follow" />';
+$endpart='<script
+			src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+			integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
+			crossorigin="anonymous"
+		></script>
+	</body>
+	<p class="text-end m-5 developer">개발자 이현희</p>
+</html>';
+?>
--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-	
+include "commonpart.php";
+if($_GET['id']=="resume"){
+	echo "how are u";
+}
 ?>
+<!DOCTYPE html>
+<html lang="ko">
+	<head>
+		<?=$headpart?>
+	</head>
+	<body>
+		<a href="index.php?id=resume">이력서</a>
+		<script
+			src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
+			integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
+			crossorigin="anonymous"
+		></script>
+	</body>
+</html>
