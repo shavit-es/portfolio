426db9832a95e9a9b1f51a9d851c8c0779e9e099
shavit-es <shavit0423@naver.com>
2021-10-22 11:59:58 +0000

    setting

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <?php
 include "commonpart.php";
 if($_GET['id']=="resume"){
-	echo "how are u";
+	
 }
 ?>
+
 <!DOCTYPE html>
 <html lang="ko">
 	<head>
 		<?=$headpart?>
 	</head>
 	<body>
-		<a href="index.php?id=resume">이력서</a>
+		<h1 class="text-center">
+			이현희
+		</h1>
+		<a href="index.php?id=resume" class="ms-5">이력서</a>
 		<script
 			src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
 			integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
 			crossorigin="anonymous"
 		></script>
 	</body>
+	<?=$endpart?>
 </html>
