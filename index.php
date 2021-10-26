59d010c46089548fc68f7297e266c27fef3d86cf
shavit-es <shavit0423@naver.com>
2021-10-26 12:13:41 +0000

    10/26

--- index.php
+++ index.php
@@ -1,7 +1,9 @@
 <?php
 include "commonpart.php";
+$resumelink='<a href="index.php?id=resume" class="ms-5">이력서</a>';
 if($_GET['id']=="resume"){
-	
+	$resumelink='';
+	$resume='';
 }
 ?>
 
@@ -14,7 +16,18 @@ if($_GET['id']=="resume"){
 		<h1 class="text-center">
 			이현희
 		</h1>
-		<a href="index.php?id=resume" class="ms-5">이력서</a>
+		<div class="d-flex justify-content-start">
+			<?=$resumelink?>
+			<?=$resume?>
+			<br>
+			<div class='border-end border-5 border-primary p-3'>
+			<img src="images/face.png" alt="My Image" class='w-30 h-40 m-5'>
+				<br>한양대학교 산업공학과 입학
+				<br>
+			</div>
+			
+		</div>
+		
 		<script
 			src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js"
 			integrity="sha384-U1DAWAznBHeqEIlVSCgzq+c9gqGAJn5c/t99JyeKa9xxaYpSvHU5awsuZVVFIhvj"
