cef0eda16caeb12c8580d8ded762720450475ab5
shavit-es <shavit0423@naver.com>
2021-10-26 22:48:00 +0000

    10.27

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<!-- https://www.miricanvas.com/templates/presentation/1wo3 -->
 <?php
 include "commonpart.php";
 $resumelink='<a href="index.php?id=resume" class="ms-5">이력서</a>';
@@ -10,20 +11,58 @@ if($_GET['id']=="resume"){
 <!DOCTYPE html>
 <html lang="ko">
 	<head>
+		<style>
+			@import url('https://fonts.googleapis.com/css2?family=Black+Han+Sans&family=Jua&display=swap');
+		</style>
 		<?=$headpart?>
 	</head>
-	<body>
+	<body class="font">
 		<h1 class="text-center">
 			이현희
 		</h1>
 		<div class="d-flex justify-content-start">
 			<?=$resumelink?>
+			<br>
 			<?=$resume?>
 			<br>
-			<div class='border-end border-5 border-primary p-3'>
-			<img src="images/face.png" alt="My Image" class='w-30 h-40 m-5'>
-				<br>한양대학교 산업공학과 입학
+			<div class='border-end border-5 border-primary p-3 w-350 text-center'>
+			<img src="images/face.png" alt="My Image" class='m-5 rounded mx-auto d-block'>
+				<br>조직의 일원이자 사업의 주인으로서 적극적으로 참여하는 개발자 이현희입니다.
 				<br>
+			</div>
+			<div class='m-5'>
+				<h2>
+					이현희 / Lee Hyun Hee
+				</h2>
+				1998. 04. 23 / 서울특별시 성동구
+				<br>
+				Email : shavit0423@naver.com
+				<h2>
+					학력
+				</h2>
+				2017.2 건국대학교사범대학부속 고등학교 졸업<br>
+				2018.3 ~ 한양대학교 산업공학과 재학 중<br>
+				<h2>
+					능력
+				</h2>
+				Python<br>
+				PHP<br>
+				MySQL<br>
+				CSS<br>
+				JavaScript<br>
+			</div>
+			<div class='m-5'>
+				<h2>
+					언어능력
+				</h2>
+				TOEIC 890<br>
+				<h2>
+					자격증
+				</h2>
+				ITQ 아래한글<br>
+				ITQ 엑셀<br>
+				컴퓨터활용능력 2급<br>
+				워드프로세서<br>
 			</div>
 			
 		</div>
